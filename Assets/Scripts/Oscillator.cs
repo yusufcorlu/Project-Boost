9d83c0e3366dccc6962479edba3da3f0b10d6653
yusufcorlu <yusufcorlu@hotmail.com>
2023-05-27 22:32:38 +0300

    .

--- Assets/Scripts/Oscillator.cs
+++ Assets/Scripts/Oscillator.cs
@@ -18,6 +18,11 @@ public class Oscillator : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        Movement();
+    }
+
+    void Movement()
+    {
         if(period <= Mathf.Epsilon){return; }
         float cycles = Time.time / period;
 
